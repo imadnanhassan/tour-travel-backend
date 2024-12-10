ff3faf088033ee0f33858daf5c9b1d23b182f6a8
imadnanhassan <abulhassan.dev@gmail.com>
2024-12-11 04:46:30 +0600

    user CURD operation done

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -6,6 +6,30 @@ const createUser = async (payload: IUser): Promise<IUser> => {
   return result;
 };
 
+const getUsers = async () => {
+  const result = await UserModel.find()
+  return result
+}
+
+const getSingleUser = async (userId: string) => {
+  const result = await UserModel.findById(userId)
+  return result
+}
+
+const updateUser = async (userId: string, payload: IUser) => {
+  const result = await UserModel.findByIdAndUpdate(userId, payload, { new: true })
+  return result
+}
+
+const deleteUser = async (userId: string) => {
+  const result = await UserModel.findByIdAndDelete(userId)
+  return result
+}
+
 export const userService = {
   createUser,
+  getUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
 };
